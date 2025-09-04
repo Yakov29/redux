05e7b617860ba09121fcec35b1a6a787f7b5b985
ZOVUT YAKOV <108018683+Yakov29@users.noreply.github.com>
2025-09-04 11:56:40 +0300

    .

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,31 @@
 import {useSelector} from "react-redux";
+import { useDispatch } from "react-redux";
 import {use} from "react";
 
 
 const Counter = () => {
 
     const value = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const addNumber = () => {
+        dispatch({
+            type: "newNumber",
+            payload: 5
+        })
+    }
+
+    const removeNumber = () => {
+        dispatch({
+            type: "oldNumber",
+            payload: 4
+        })
+    }
     return (
         <section className={"counter"}>
         <p className={"value"}>{value}</p>
-        <button className={"button-add"}>+</button>
-         <button className={"button-remove"}>-</button>
+        <button onClick={addNumber} className={"button-add"}>+</button>
+         <button onClick={removeNumber} className={"button-remove"}>-</button>
         </section>
 )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,18 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
+import { devToolsEnhancer } from "@redux-devtools/extension";
 
 const rootReducer = (state = 0, action) => {
+
+    console.log(action)
+    if (action.type === "newNumber") {
+        state = state + 1
+    } else if (action.type === "oldNumber") {
+        // state = action.payload
+        state = state - 1
+    }
     return state
 }
 
-export const store = createStore(rootReducer)
+
+const enhancer = devToolsEnhancer()
+export const store = createStore(rootReducer, enhancer)
